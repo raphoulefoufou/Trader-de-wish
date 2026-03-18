70a8623e299467bce358764055823fa68d220a0c
Raphael Capristo <pastagraphael@gmail.com>
2026-03-18 11:00:40 +0100

    ajdehiorjhfruo

--- valider.php
+++ valider.php
@@ -14,18 +14,31 @@ $solde=$_SESSION['client']['monnaie'];
 $id_client=$_SESSION['client']['id'];
 
 
-if($solde>=$prix_total){
- $sql = $pdo->prepare("
-    INSERT INTO mes_actions (id_marche, id_client, nom, prix_total)
-    VALUES (:id_marche, :id_client, :nom, :prix_total)
-");
+$query = $pdo->prepare("SELECT monnaie FROM client WHERE id = ?");
+$query->execute([$id_client]);
+$row = $query->fetch(PDO::FETCH_ASSOC);
 
-$sql->execute([
-    ':id_marche' => $id_marche,
-    ':id_client' => $id_client,
-    ':nom' => $nom,
-    ':prix_total' => $prix_total
-]);
+
+if($row){
+    $monnaie=$row['monnaie'];
+}
+
+if ($solde >= $prix_total) {
+    $sql = $pdo->prepare("
+        INSERT INTO mes_actions (id_marche, id_client, nom, prix_total)
+        VALUES (:id_marche, :id_client, :nom, :prix_total)
+    ");
+    
+    $sql->execute([
+        ':id_marche' => $id_marche,
+        ':id_client' => $id_client,
+        ':nom'       => $nom,
+        ':prix_total' => $prix_total
+    ]);
+$nouveau_solde = $monnaie - $prix_total;
+
+$sql2 = $pdo->prepare("UPDATE client SET monnaie = ? WHERE id = ?");
+$sql2->execute([$nouveau_solde, $id_client]);
 
 echo '1';
 }else{
--- wallet.php
+++ wallet.php
@@ -9,32 +9,73 @@
 </head>
 <body>
 <?php
-include 'header.php'
+include 'bd.php';
+include 'header.php';
+session_start();
+$pdo=getBD();
+if(isset($_SESSION['client'])){
+$id_client=$_SESSION['client']['id'];
+
+
+
+
+$stmt=$pdo->prepare("SELECT * FROM mes_actions WHERE id_client =?");
+$stmt->execute([$id_client]);
+  $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$sql=$pdo->prepare("SELECT SUM(prix_total) as total FROM mes_actions WHERE id_client= ?");
+$sql->execute([$id_client]);
+$result=$sql->fetch(PDO::FETCH_ASSOC);
+$total_portefeuille = $result['total'] ?? 0;
 ?>
+  
 <div class="wallet-container">
     <h1>Porte Feuille</h1>
-    <h2>Votre porte feuille actuel est de :</h2>
-
-    <div class="wallet-card">
-        <p>10 000 €</p>
-    </div>
+    <h2>Votre porte feuille actuel est de : <?php echo($total_portefeuille) ?> €</h2>
 </div>
+<table class="marches"> 
+    <h2>Vos actions </h2>
+    <tr>
 
+<th>Nom du Marche</th>
+<th>Valeur</th>
+<th>Pourcentages</th>
+</tr>
+<?php
+foreach($row as $march){
 
-<div class="actions-card">
-    <h3>Vos actions</h3>
+$prix = $march['prix_total'];
 
-    <div class="action-row">
-        <p class="action-name">Thales</p>
-        <p class="action-price">9000 €</p>
-    </div>
+$pourcentage = 0;
 
-    <div class="action-row">
-        <p class="action-name">Total Energy</p>
-        <p class="action-price">1000 €</p>
-    </div>
+if($total_portefeuille > 0){
+$pourcentage = ($prix / $total_portefeuille) * 100;
+}
 
-</div>
+echo "<tr>
+<td>{$march['nom']}</td>
+<td>{$march['prix_total']} €</td>
+<td>".round($pourcentage,2)." %</td>
+</tr>";
+$labels[] = $march['nom'];
+    $valeurs[] = round($pourcentage, 2);
+}
+
+?>
+
+</table>
+<?php
+}?>
+
+<?php
+if(!isset($_SESSION['client'])) {?>
+
+        <p class="login-message">
+            Vous devez être connecté pour voir votre portfolio.
+        </p>
+<?php
+}
+?>
+
 
 
 </body>
